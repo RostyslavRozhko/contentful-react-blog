20807c6aea8323fa6e8c576ddefc74573a419740
rostyslav <rostyslav.rozhko@gmail.com>
2017-05-31 10:20:44 +0300

    added spinner

--- src/containers/Loading.js
+++ src/containers/Loading.js
@@ -1,8 +1,17 @@
 import React from "react";
+import CircularProgress from "material-ui/CircularProgress";
 
 const Loading = () => (
-  <div>
-    Loading
+  <div
+    style={{
+      width: "100%",
+      height: "100%",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center"
+    }}
+  >
+    <CircularProgress />
   </div>
 );
 
